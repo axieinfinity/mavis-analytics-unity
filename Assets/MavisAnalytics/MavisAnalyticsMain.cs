93615b8584c4fc744986433fa87921f18927f11d
Chafik Naceri <113873839+chafikaxie@users.noreply.github.com>
2023-04-28 13:13:27 +0400

    [SDK Exporter] Develop Main SDK Exporter

---
+++ Assets/MavisAnalytics/MavisAnalyticsMain.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MavisAnalytics
+{
+    public class MavisAnalyticsMain
+    {
+        public static string VersionName = "1.0.0";
+    }
+}
