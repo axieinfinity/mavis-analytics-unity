632669faec1cfd9c8f18bcb5de827382858407f7
Chafik Naceri <113873839+chafikaxie@users.noreply.github.com>
2023-05-18 11:03:16 +0400

    [Analytics Tracking] Fix Memory Leak using Web Requests

--- Assets/MavisAnalytics/Scripts/Utils/AnalyticsServer.cs
+++ Assets/MavisAnalytics/Scripts/Utils/AnalyticsServer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
@@ -8,17 +9,25 @@ using UnityEngine.Events;
 using UnityEngine.Networking;
 using HttpMethod = System.Net.Http.HttpMethod;
 
-
 namespace MavisAnalyticsSDK
 {
     public static class AnalyticsServer
     {
         public static void PostAPI(string url, MavisAnalyticsRequest data, UnityAction<Error, string> cb)
         {
-            Request(url, HttpMethod.Post, data, cb);
+            try
+            {
+                UnityWebRequest request = Request(url, HttpMethod.Post, data, cb);
+                AnalyticsRunner.StartCoroutine(SendWebRequest(request, cb));
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("Exception occurred while preparing to send analytics request: " + ex.Message);
+                cb(Error.New(-1, ex.Message), null);
+            }
         }
 
-        private static void Request(string url, HttpMethod method, MavisAnalyticsRequest data, UnityAction<Error, string> callback)
+        private static UnityWebRequest Request(string url, HttpMethod method, MavisAnalyticsRequest data, UnityAction<Error, string> callback)
         {
             var request = new UnityWebRequest(url, "POST");
 
@@ -26,26 +35,38 @@ namespace MavisAnalyticsSDK
             request.SetRequestHeader("Authorization", "Basic " + $"{MavisAnalytics.GetApiKey()}:".ToBase64());
             request.method = method.ToString().ToUpper();
             request.downloadHandler = new DownloadHandlerBuffer();
-            var json = JsonConvert.SerializeObject(data, serializerSettings);
-            var jsonBytes = new UTF8Encoding().GetBytes(json);
-            request.uploadHandler = new UploadHandlerRaw(jsonBytes);
 
-            AnalyticsRunner.StartCoroutine(SendWebRequest(request, callback));
+            try
+            {
+                var json = JsonConvert.SerializeObject(data, serializerSettings);
+                var jsonBytes = new UTF8Encoding().GetBytes(json);
+                request.uploadHandler = new UploadHandlerRaw(jsonBytes);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("Exception occurred while serializing analytics request: " + ex.Message);
+                callback(Error.New(-1, ex.Message), null);
+            }
 
+            return request;
         }
 
-        private static IEnumerator SendWebRequest(UnityWebRequest wr, UnityAction<Error, string> onComplete)
+        private static IEnumerator SendWebRequest(UnityWebRequest request, UnityAction<Error, string> onComplete)
         {
-            yield return wr.SendWebRequest();
+            using (request)
+            {
+                yield return request.SendWebRequest();
 
-            if (wr.responseCode != 200 || !string.IsNullOrEmpty(wr.error))
-            {
-                onComplete(Error.New(wr.responseCode, wr.error), null);
-                Debug.Log("Event Failed to  send");
-            } else
-            {
-                onComplete(null, wr.downloadHandler?.text);
-                Debug.Log("Event Successfully sent" + wr.downloadHandler?.text);
+                if (request.responseCode != 200 || !string.IsNullOrEmpty(request.error))
+                {
+                    onComplete(Error.New(request.responseCode, request.error), null);
+                    Debug.Log("Event Failed to  send");
+                }
+                else
+                {
+                    onComplete(null, request.downloadHandler?.text);
+                    Debug.Log("Event Successfully sent" + request.downloadHandler?.text);
+                }
             }
         }
 
@@ -80,4 +101,3 @@ namespace MavisAnalyticsSDK
         }
     }
 }
-
