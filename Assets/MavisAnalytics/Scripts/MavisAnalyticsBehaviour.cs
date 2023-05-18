d96b87c5aff9be5ae5464c0c32472e0b13c65e5d
Chafik Naceri <113873839+chafikaxie@users.noreply.github.com>
2023-05-18 12:15:17 +0400

    Clean MavisAnalyticsBehaviour

--- Assets/MavisAnalytics/Scripts/MavisAnalyticsBehaviour.cs
+++ Assets/MavisAnalytics/Scripts/MavisAnalyticsBehaviour.cs
@@ -1,43 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-namespace MavisAnalyticsSDK
+using MavisAnalyticsSDK;
+using UnityEngine.SceneManagement;
+public class MavisAnalyticsBehaviour : MonoBehaviour
 {
-    public class MavisAnalyticsBehaviour : MonoBehaviour
+    private void Awake()
     {
-        private void Awake()
-        {
-            MavisAnalytics.InitAnalytics();
-        }
-        private void Start()
-        {
-            MavisAnalytics.SetUserId("random_user_test_id");
-        }
-
-        public void TrackEv(string eventName)
-        {
-            MavisAnalytics.TrackEvent(eventName,"test");
-        }
-
-        public void StartGame(string eventName)
-        {
-            Dictionary<string, object> metadata = new Dictionary<string, object>()
-            {
-                {"level", 1 }
-            };
-            MavisAnalytics.TrackEvent(eventName, "progression", metadata);
-        }
-        public void GameEnded(string eventName)
-        {
-            Dictionary<string, object> metadata = new Dictionary<string, object>()
-            {
-                {"level", 1 },
-                {"won", false },
-                { "items" , new List<string>(){"weapon1","weapon2" } }
-            };
-            MavisAnalytics.TrackEvent(eventName, "progression", metadata);
-        }
+        MavisAnalytics.InitAnalytics();
+        DontDestroyOnLoad(gameObject);
     }
+    private void Start()
+    {
+        // Make sure to Set the actual User id on Login
+        string randUserId = System.Guid.NewGuid().ToString();
+        MavisAnalytics.SetUserId(randUserId);
+    }
+ 
 }
+
 
